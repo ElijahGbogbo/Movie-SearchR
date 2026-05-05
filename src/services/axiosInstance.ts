ba23936808eb9a0cdc9ea5fd31f82c9bbd6beabd
Elijah Gbogbo <gbogboelijah@gmail.com>
2026-05-05 22:41:48 +0100

    Fixed code formatting

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 const api = axios.create({
-    baseURL: "https://api.themoviedb.org/3"
+    baseURL: "https://api.themoviedb.org/3", 
     headers: {
         accept: "application/json", 
         Authorization: `Bearer ${API_TOKEN}`
