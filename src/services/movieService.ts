80720e948c7bb87bb645c9aa9329536760d9cb6a
Elijah Gbogbo <gbogboelijah@gmail.com>
2026-05-05 22:36:00 +0100

    Created an axios instance for a central api layer and created an api to get Popular movies

---
+++ src/services/movieService.ts
@@ -0,0 +1,11 @@
+import api from "./axiosInstance.ts";
+
+const getPopularMovies = async () => {
+    try {
+        const {results} = await api.get("/movie/popular");
+        console.log(results);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
