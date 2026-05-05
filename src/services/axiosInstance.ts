2a66e86cafeb13884fba613ae922308ae5de3c78
Elijah Gbogbo <gbogboelijah@gmail.com>
2026-05-05 22:52:28 +0100

    Fixed coding style

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from 'axios'
 
-const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN
 
 const api = axios.create({
-    baseURL: "https://api.themoviedb.org/3", 
-    headers: {
-        accept: "application/json", 
-        Authorization: `Bearer ${API_TOKEN}`
-    }
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_TOKEN}`,
+  },
 })
 
-export api;
+export default api;
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,11 +1,10 @@
-import api from "./axiosInstance.ts";
+import api from './axiosInstance.ts'
 
 const getPopularMovies = async () => {
-    try {
-        const {results} = await api.get("/movie/popular");
-        console.log(results);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const { results } = await api.get('/movie/popular')
+    console.log(results)
+  } catch (error) {
+    console.log(error)
+  }
 }
-
