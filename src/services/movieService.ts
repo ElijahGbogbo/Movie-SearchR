d05df0eb4252d3d6123c6346fcb7091ccb9f41a8
Elijah Gbogbo <gbogboelijah@gmail.com>
2026-05-24 19:40:34 +0100

    Fixed code structure in movieService.ts and test.ts

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,12 +1,8 @@
 import api from './axiosInstance.ts'
 
 const getPopularMovies = async (): Promise<void> => {
-  try {
     const { data } = await api.get('/movie/popular')
-    // console.log(data)
-  } catch (error) {
-    console.log(error)
-  }
+    return data
 }
 
 export default getPopularMovies
--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,8 @@
 import api from './services/axiosInstance.ts'
 
 const getPopularMovies = async (): Promise<void> => {
-  try {
     const { data } = await api.get('/movie/popular')
     console.log(data)
-  } catch (error) {
-    console.log(error)
-  }
 }
 
 getPopularMovies()
