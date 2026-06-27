1908f1ad40c900dc6c4a8655db7af01c4dbd79c4
Elijah Gbogbo <gbogboelijah@gmail.com>
2026-06-27 23:59:18 +0100

    Re-wrote the code in MovieContext.tsx to make Favorite movies survive browser refreshes

--- src/contexts/MovieContext.tsx
+++ src/contexts/MovieContext.tsx
@@ -14,12 +14,10 @@ export const useMovieContext = () => {
 }
 
 export const MovieProvider = ({ children }: MovieProviderProps) => {
-  const [favorites, setFavorites] = useState<Movie[]>([])
-
-  useEffect(() => {
+  const [favorites, setFavorites] = useState<Movie[]>(() => {
     const storedFavs = localStorage.getItem('favorites')
-    if (storedFavs) setFavorites(JSON.parse(storedFavs))
-  }, [])
+    return storedFavs ? JSON.parse(storedFavs) : []
+  })
 
   useEffect(() => {
     localStorage.setItem('favorites', JSON.stringify(favorites))
