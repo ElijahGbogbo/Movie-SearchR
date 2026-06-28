ba2a4958fd32da7f5d71fde04671c1009ff46dff
Elijah Gbogbo <gbogboelijah@gmail.com>
2026-06-28 01:17:24 +0100

    Fixed the formatting across my code base

--- src/components/MovieCard.test.tsx
+++ src/components/MovieCard.test.tsx
@@ -1,21 +1,19 @@
 import { render, screen } from '@testing-library/react'
 import MovieCard from './MovieCard.tsx'
 import type { Movie } from '../types/Movie.ts'
-import { MovieProvider } from "../contexts/MovieContext.tsx"
+import { MovieProvider } from '../contexts/MovieContext.tsx'
 
 const mockMovie: Movie = {
   id: 1,
   title: 'Batman',
   release_date: '2019',
-  poster_path: 'https://via.placeholder.com/300x450',      
+  poster_path: 'https://via.placeholder.com/300x450',
 }
 
 test('renders movie title', () => {
   render(
     <MovieProvider>
-      <MovieCard
-      movie={mockMovie}
-      />
+      <MovieCard movie={mockMovie} />
     </MovieProvider>
   )
   expect(screen.getByText('Batman')).toBeInTheDocument()
